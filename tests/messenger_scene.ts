361e74ddb581a834f473f8a2619110e681034ada
vadkou <contact@vadkou.me>
2018-10-29 18:37:38 +0700

    - Internal type system for the Scenes Scene describe what does the user have available to access certain features
     - 2 example scenes defined with the internal structure

---
+++ tests/messenger_scene.ts
@@ -0,0 +1,70 @@
+import {Area, AreaType, Entity, Field, Interaction, SceneDescription, withPriority} from '../src/types/logic';
+
+export function createMessengerScene(): SceneDescription {
+    const name: Field = {
+        key: 'name',
+        mutable: false,
+        type: 'text',
+        size: 'small',
+    };
+
+    const avatar: Field = {
+        key: 'avatar',
+        mutable: false,
+        type: 'image',
+        size: 'small',
+    };
+
+    const content: Field = {
+        key: 'content',
+        mutable: true,
+        type: 'text',
+        size: 'large',
+    };
+
+    const messageEntity: Entity = {
+        key: 'message_type',
+        fields: [
+            withPriority(avatar, 1),
+            withPriority(name, 3),
+            withPriority(content, 3),
+        ],
+    };
+
+    const browseInteraction: Interaction = {
+        entity: messageEntity,
+        primaryAction: 'browse',
+        actions: ['edit', 'delete'],
+    };
+
+    const sendInteraction: Interaction = {
+        entity: messageEntity,
+        primaryAction: 'add',
+        actions: ['edit'],
+    };
+
+    const navBar: Area = {
+        key: 'nav_bar',
+        type: AreaType.NAVIGATE,
+    };
+    const messagesList: Area = {
+        key: 'messages_list',
+        type: AreaType.INTERACT,
+        interaction: browseInteraction,
+    };
+    const messageInput: Area = {
+        key: 'message_input',
+        type: AreaType.INTERACT,
+        interaction: sendInteraction,
+    };
+
+    return {
+        key: 'messages_screen',
+        areas: [
+            withPriority(navBar, 1),
+            withPriority(messagesList, 5),
+            withPriority(messageInput, 3),
+        ],
+        connectedScenes: [],
+    };
+}
---
+++ tests/shop_catalogue_scene.ts
@@ -0,0 +1,80 @@
+import {Area, AreaType, Entity, Field, Interaction, SceneDescription, withPriority} from '../src/types/logic';
+
+export function createCatalogueScene(): SceneDescription {
+    const title: Field = {
+        key: 'name',
+        mutable: false,
+        type: 'text',
+        size: 'small',
+    };
+
+    const price: Field = {
+        key: 'price',
+        mutable: false,
+        type: 'number',
+        size: 'small',
+    };
+
+    const amountLeft: Field = {
+        key: 'amount_left',
+        mutable: false,
+        type: 'number',
+        size: 'small',
+    };
+
+    const photo: Field = {
+        key: 'photo',
+        mutable: false,
+        type: 'image',
+        size: 'large',
+    };
+
+    const description: Field = {
+        key: 'description',
+        mutable: false,
+        type: 'text',
+        size: 'large',
+    };
+
+    const itemEntity: Entity = {
+        key: 'message_type',
+        fields: [
+            withPriority(title, 3),
+            withPriority(photo, 3),
+            withPriority(price, 3),
+            withPriority(amountLeft, 1),
+            withPriority(description, 1),
+        ],
+    };
+
+    const browseInteraction: Interaction = {
+        entity: itemEntity,
+        primaryAction: 'browse',
+        actions: ['read'],
+    };
+
+    const navBar: Area = {
+        key: 'nav_bar',
+        type: AreaType.NAVIGATE,
+    };
+    const itemList: Area = {
+        key: 'item_list',
+        type: AreaType.INTERACT,
+        interaction: browseInteraction,
+    };
+    const filters: Area = {
+        key: 'item_filters',
+        type: AreaType.OPTIONS,
+        fields: [withPriority(price, 1)],
+    };
+
+    return {
+        key: 'messages_screen',
+        areas: [
+            withPriority(navBar, 1),
+            withPriority(itemList, 5),
+            withPriority(filters, 1),
+        ],
+        connectedScenes: [],
+    };
+}
