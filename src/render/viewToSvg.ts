30a97fafb23a5ef749f0e55178c9af0097ccc1a0
vadkou <contact@vadkou.me>
2018-10-30 00:44:43 +0700

    - Calculating the layout with Yoga  - Tweaked SVG render to account for views' relative positions

--- src/render/viewToSvg.ts
+++ src/render/viewToSvg.ts
@@ -7,31 +7,52 @@ const document = window.document;
 
 import {View} from '../types/inflate';
 
+interface Position {
+    top: number;
+    left: number;
+}
+
+const defaultPosition = {
+    top: 0,
+    left: 0,
+};
+
+function addOffset(position: Position, top: number, left: number): Position {
+    return {
+        top: position.top + top,
+        left: position.left + left,
+    };
+}
+
 // TODO: DAEMON: Create separate document elements for each render
 export default class ViewRenderer {
     private draw: svgjs.Doc = SVG(document.documentElement);
 
-    public renderView(view: View): string {
+    public renderView(view: View, offset: Position = defaultPosition): string {
         const { width, height, top, left } = view.layout;
+
+        const position = addOffset(offset, top, left);
 
         const rect = this.draw
             .rect(width, height)
-            .move(top, left);
+            .move(position.left, position.top);
 
         switch (view.type) {
             case 'solid':
+            case 'text':
                 rect.fill('black')
-                    .opacity(0.5);
+                    .opacity(0.1);
                 break;
-            default:
-                rect.stroke({
+            case 'wire':
+                rect.fill('#00000000')
+                    .stroke({
                     color: 'black',
-                    width: 4,
-                    opacity: 0.5,
+                    width: 8,
+                    opacity: 0.25,
                 });
         }
 
-        view.children.forEach(this.renderView);
+        view.children.forEach((it) => this.renderView(it, position));
 
         return this.draw.svg();
     }
